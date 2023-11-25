fa8f24df8700d8dde350879e5035b2a2db27ed9d
wtks <30363887+wtks@users.noreply.github.com>
2023-11-25 10:20:03 +0900

    sql

---
+++ app/sql/initdb.d/10_schema.sql
@@ -0,0 +1,106 @@
+USE `isupipe`;
+
+-- ユーザ (配信者、視聴者)
+CREATE TABLE `users` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL,
+  `display_name` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `description` TEXT NOT NULL,
+  UNIQUE `uniq_user_name` (`name`)
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- プロフィール画像
+CREATE TABLE `icons` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `image` LONGBLOB NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ユーザごとのカスタムテーマ
+CREATE TABLE `themes` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `dark_mode` BOOLEAN NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ライブ配信
+CREATE TABLE `livestreams` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `description` text NOT NULL,
+  `playlist_url` VARCHAR(255) NOT NULL,
+  `thumbnail_url` VARCHAR(255) NOT NULL,
+  `start_at` BIGINT NOT NULL,
+  `end_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ライブ配信予約枠
+CREATE TABLE `reservation_slots` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `slot` BIGINT NOT NULL,
+  `start_at` BIGINT NOT NULL,
+  `end_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ライブストリームに付与される、サービスで定義されたタグ
+CREATE TABLE `tags` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL,
+  UNIQUE `uniq_tag_name` (`name`)
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ライブ配信とタグの中間テーブル
+CREATE TABLE `livestream_tags` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `livestream_id` BIGINT NOT NULL,
+  `tag_id` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ライブ配信視聴履歴
+CREATE TABLE `livestream_viewers_history` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ライブ配信に対するライブコメント
+CREATE TABLE `livecomments` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  `comment` VARCHAR(255) NOT NULL,
+  `tip` BIGINT NOT NULL DEFAULT 0,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ユーザからのライブコメントのスパム報告
+CREATE TABLE `livecomment_reports` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  `livecomment_id` BIGINT NOT NULL,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- 配信者からのNGワード登録
+CREATE TABLE `ng_words` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  `word` VARCHAR(255) NOT NULL,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE INDEX ng_words_word ON ng_words(`word`);
+
+-- ライブ配信に対するリアクション
+CREATE TABLE `reactions` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  -- :innocent:, :tada:, etc...
+  `emoji_name` VARCHAR(255) NOT NULL,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
