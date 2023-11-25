6928326ac5894c27c6dd8c135e3bcce1ef5b452b
tohutohu <tohu.soy@gmail.com>
2023-11-25 10:46:04 +0900

    INDEX

--- app/sql/initdb.d/10_schema.sql
+++ app/sql/initdb.d/10_schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE `users` (
 CREATE TABLE `icons` (
   `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` BIGINT NOT NULL,
-  `image` LONGBLOB NOT NULL
+  `image` LONGBLOB NOT NULL,
+  ADD INDEX `idx_user_id` (`user_id`)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- ユーザごとのカスタムテーマ
