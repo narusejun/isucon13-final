145a73e2ce59f73225a5df1eaed9bc0569a59bb6
tohutohu <tohu.soy@gmail.com>
2023-11-27 23:27:06 +0900

    DNSちゃんと返す

--- app/go/dns.go
+++ app/go/dns.go
@@ -1338,11 +1338,10 @@ func startDNS() error {
 					newRR(r.Question[0].Name + " 120 IN A 54.178.156.176"),
 				}
 			} else {
-				return
-				// m.Rcode = dns.RcodeNameError
-				// m.Ns = []dns.RR{
-				// 	newRR("u.isucon.dev. 60 IN SOA ns1.u.isucon.dev. hostmaster.u.isucon.dev. 0 10800 3600 604800 3600"),
-				// }
+				m.Rcode = dns.RcodeNameError
+				m.Ns = []dns.RR{
+					newRR("u.isucon.dev. 0 IN SOA ns1.u.isucon.dev. hostmaster.u.isucon.dev. 0 10800 3600 604800 3600"),
+				}
 			}
 		}
 		w.WriteMsg(m)
